b5e624fc54b421a8152c7cb60fb7976466b90fbd
Daniel Vahsholtz <106500790+danvahsh@users.noreply.github.com>
2022-12-26 12:13:09 -0700

    Fixed Grammar

--- MCusernamePY.py
+++ MCusernamePY.py
@@ -4,7 +4,7 @@ from mojang import MojangAPI
 for word in english_words_lower_alpha_set:
     uuid = MojangAPI.get_uuid(word)
     if not uuid:
-        print("\n", word, "is not taken", "\n")
+        print("\n", word, "is probably not taken", "\n")
         list = open("avaliable.usernames", "a")
         current_word = word + "\n"
         list.write(current_word)
